126571daaf381ddfa8a6c7c950f9cba472813ff0
Nuria <nuriagomez.estudios@gmail.com>
2025-01-27 14:25:51 +0100

    Api jikan anime.php

--- 08-apis/Jikan/top_anime.php
+++ 08-apis/Jikan/top_anime.php
@@ -3,7 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Top animes</title>
+    <?php
+    error_reporting( E_ALL );
+    ini_set("display_errors", 1 );
+    ?>
 </head>
 <body>
     <?php
@@ -18,12 +23,35 @@
      $datos = json_decode($respuesta, true);
      $animes=$datos["data"];
      ?>
-     <ol>
-        <?php
-        foreach($animes as $anime){ ?>
-        <li><?php echo $anime["title"]?></li>
-        <?php } ?>
-        
-     </ol>
+
+<div class="container">
+<table class='table table-striped table-hover table-sm'>
+        <thead class='table-dark'>
+            <tr>
+                <th>Posición:</th>
+                <th>Titulo:</th>
+                <th>Nota:</th>
+                <th>Imágen:</th>
+
+            </tr>
+        </thead>
+         <tbody>
+            <?php
+                foreach($animes as $anime){?>
+            <tr>
+                <td><?php echo $anime["rank"] ?></td>
+                <td>
+                    <a href="anime.php?id=<?php echo $anime["mal_id"]?>">
+                        <?php echo $anime["title"]?>
+                    </a>
+                </td>
+                <td><?php echo $anime["score"] ?></td>
+                <td><img width="100px"src= "<?php echo $anime["images"]["jpg"]["image_url"]?>"></img></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+ </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
